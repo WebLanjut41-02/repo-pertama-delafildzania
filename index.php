a1c79acac12b2ef5354999ce98f766766a3152bf
delafildzania <42663150+delafildzania@users.noreply.github.com>
2019-01-18 10:26:18 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,66 @@
-<?php
-class Kelas{
-    protected $jumlah;
-    public function belajar(){
-       echo "Sedang belajar <br>";
-    }
-    public function tampil_jumlah(){
-        echo "Jumlah mahasiswa adalah ".$this->jumlah."<br>";
-    }
-}
-class StudyGroup extends Kelas{
-    public function setjumlah($jumlah){
-        $this->jumlah = $jumlah;
-    }
-}
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BMI OOP</title>
+	<?php require("Kalkulator.php"); ?>
+</head>
+<body>
+	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+	 Tinggi<input type="text" name="tinggi"><br>
+	 Berat<input type="text" name="berat"><br>
+	 Jenis Kelamin<br>
+	 				<input type="radio" name="jk" value="Laki" id="l">
+					 <label for="l">Laki-laki</label>
+				  	<br>
+	 				<input type="radio" name="jk" value="Perempuan" id="p">
+	 				<label for="p">Perempuan</label>
+	 				<br>
+	 <input type="submit" name="submit">
 
-$d34102 = new StudyGroup();
-//$d34102->jumlah = 5;
-$d34102->setjumlah(20);
-$d34102->tampil_jumlah();
+
+</body>
+</html>
+
+<?php
+
+$hasil= new Kalkulator();
+
+if(isset($_POST['submit'])){
+$hasil->bmi($_POST['tinggi'], $_POST['berat'], $_POST['jk']);
+	echo $hasil->bmi();
+}
+    
+
+// if($_POST['jk'] == 'Laki'){
+// 	if($_POST['jk'] == 'Laki' < 17) {
+// 	        echo "Underweight/Kurus";
+// 	        $hasil->bmi($tinggi, $berat);
+// 	    }elseif ($Laki < 23 ) {
+// 	        echo "Normal";
+// 	        $hasil->bmi($tinggi, $berat);
+// 	    }elseif ($Laki < 27 ) {
+// 	        echo "Overweight";
+// 	        $hasil->bmi($tinggi, $berat);
+// 	    }elseif ($Laki > 27 ) {
+// 	        echo "Obesitas";
+// 	        $hasil->bmi($tinggi, $berat);
+// 	    }	
+// }
+
+// if($_POST['jk'] == 'Perempuan'){
+//  if($_POST['jk'] == 'Perempuan' < 18) {
+//         echo "Underweight/Kurus";
+//         $hasil->bmi($tinggi, $berat);
+//     }elseif ($Perempuan < 25 ) {
+//         echo "Normal";
+//         $hasil->bmi($tinggi, $berat);
+//     }elseif ($Perempuan < 27 ) {
+//         echo "Overweight";
+//         $hasil->bmi($tinggi, $berat);
+//     }elseif ($Perempuan > 27 ) {
+//         echo "Obesitas";
+//         $hasil->bmi($tinggi, $berat);
+//     }
+// }
+
 ?>
